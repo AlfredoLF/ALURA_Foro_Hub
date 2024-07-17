84e3e31dbc1a5df9bebe026a9ed4c0e76ea4b754
AlfredoLF <82850657+AlfredoLF@users.noreply.github.com>
2024-07-16 19:50:14 -0500

    Proyecto "Foro Hub"
    Se adiciona la carpeta "target".

---
+++ target/classes/db/migration/V10__insert_users_testing.sql
@@ -0,0 +1,5 @@
+INSERT INTO user(code, username, email, password,type_profile)
+VALUES(1, 'tlozada', 't.lozada@alura.com', 'Tewpq$Swulg/TiEpZ$2r84QciEpZ$2r04edjy$10Tewpq8iViBZ2mQ3sQxMq4viFJq/r04edj6TT84Qc', 1);
+
+INSERT INTO user(code, username, email, password,type_profile)
+VALUES(2, 'ylopez', 'y.lopez@alura.com', 'Tewpq$Swulg/TiEpZ$2r84QciEpZ$2r04edjy$10Tewpq8iViBZ2mQ3sQxMq4viFJq/r04edj6TT84Qc', 2);
